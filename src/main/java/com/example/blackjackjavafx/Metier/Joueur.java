ed34c37805dc4547a923883e2c0ffe5c7ac9b738
bendahmaney <yanis.bendahmane@etu.umontpellier.fr>
2023-11-15 14:19:22 +0100

    ajout classes Carte, Client, Joueur, Participant et Pioche. Debut de jeu et controleur jeu

---
+++ src/main/java/com/example/blackjackjavafx/Metier/Joueur.java
@@ -0,0 +1,11 @@
+package com.example.blackjackjavafx.Metier;
+
+public class Joueur extends Participant{
+
+    private String nomJoueur;
+
+    public Joueur(String nom){
+        super();
+        this.nomJoueur = nom;
+    }
+}
