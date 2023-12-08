22f3c3cb05a610fb246cc57158fe2b099db71531
GARCIA Clément <clix.mods.cod@gmail.com>
2023-12-08 16:23:26 +0100

    Implement password secure, phone number tcheck

--- src/main/java/com/example/blackjackjavafx/Application/helper/InscriptionHelper.java
+++ src/main/java/com/example/blackjackjavafx/Application/helper/InscriptionHelper.java
@@ -3,6 +3,8 @@ package com.example.blackjackjavafx.Application.helper;
 import com.example.blackjackjavafx.Application.Service.ClientService;
 import com.example.blackjackjavafx.Metier.Client;
 
+import java.util.regex.Pattern;
+
 public class InscriptionHelper
 {
     public static boolean isLoginAvailable(String login)
@@ -19,4 +21,25 @@ public class InscriptionHelper
         Client client = clientService.getClientByMail(mail);
         return client == null;
     }
+
+    public static boolean isValidPhoneNumber(String phoneNumber) {
+        // Vérifie si le numéro de téléphone n'est pas nul et a une longueur minimale
+        if (phoneNumber == null || phoneNumber.length() < 10) {
+            return false;
+        }
+
+        // Vérifie si le numéro de téléphone contient uniquement des chiffres
+        if (!isNumeric(phoneNumber)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isNumeric(String str) {
+        // Vérifie si la chaîne est composée uniquement de chiffres
+        return Pattern.compile("\\d+").matcher(str).matches();
+    }
+
+
 }
