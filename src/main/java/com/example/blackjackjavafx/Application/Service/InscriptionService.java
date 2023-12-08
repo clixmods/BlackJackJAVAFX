a0f480ae17abeda8c98b311f4a7272cdd95ef209
GARCIA Clément <clix.mods.cod@gmail.com>
2023-12-08 14:33:35 +0100

    WIP inscription check les valeurs données

---
+++ src/main/java/com/example/blackjackjavafx/Application/Service/InscriptionService.java
@@ -0,0 +1,15 @@
+package com.example.blackjackjavafx.Application.Service;
+
+public class InscriptionService
+{
+    public boolean IsLoginAvailable(String login)
+    {
+        // TODO
+        return false;
+    }
+
+    public boolean IsMailAvailable(String mail) {
+        // todo
+        return false;
+    }
+}
---
+++ src/main/java/com/example/blackjackjavafx/Application/Service/PasswordService.java
@@ -0,0 +1,9 @@
+package com.example.blackjackjavafx.Application.Service;
+
+public class PasswordService {
+    public boolean IsSecure(String password) {
+        // TODO
+
+        return false;
+    }
+}
