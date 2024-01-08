80154c717def5e2935e33b13a04ea1711dd65ee0
Elliot B <119609060+Zorglub999@users.noreply.github.com>
2024-01-08 12:07:27 +0100

    Ajout du header et des paramètres, correction de l'affichage

---
+++ src/main/java/com/example/blackjackjavafx/Application/controller/ControleurHeader.java
@@ -0,0 +1,69 @@
+package com.example.blackjackjavafx.Application.controller;
+
+import com.example.blackjackjavafx.Vue.SceneHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.InputStream;
+
+public class ControleurHeader {
+
+    private static SceneHandler sceneHandler;
+    @FXML
+    private ImageView boutonHome;
+    @FXML
+    private Label labelNomCompte;
+    @FXML
+    private Label labelArgentCourant;
+
+    @FXML
+    private ImageView boutonSettings;
+
+
+    public void initialiserHeader(SceneHandler sceneHandlerp){
+
+        if(sceneHandler == null) {
+            this.sceneHandler = sceneHandlerp;
+        }
+
+        InputStream inputStream = getClass().getResourceAsStream("/images.header/home.png");
+        if(inputStream != null) {
+            Image homeImage = new Image(inputStream);
+            boutonHome.setImage(homeImage);
+            boutonHome.setFitWidth(25);
+            boutonHome.setFitHeight(25);
+        }
+
+        InputStream inputStream2 = getClass().getResourceAsStream("/images.header/settings.png");
+        if(inputStream != null) {
+            Image settingsImage = new Image(inputStream2);
+            boutonSettings.setImage(settingsImage);
+            boutonSettings.setFitWidth(25);
+            boutonSettings.setFitHeight(25);
+        }
+
+
+
+    }
+    @FXML
+    public void handleHome() {
+        sceneHandler.afficherAccueil();
+    }
+
+    @FXML
+    public void handleSettings() {
+        sceneHandler.afficherSettings();
+    }
+
+    public void setNomCompte(String nomCompte) {
+        labelNomCompte.setText(nomCompte);
+    }
+
+    public void setArgent(double argent) {
+        labelArgentCourant.setText(argent + "$");
+    }
+}
--- src/main/java/com/example/blackjackjavafx/Vue/SceneHandler.java
+++ src/main/java/com/example/blackjackjavafx/Vue/SceneHandler.java
@@ -15,23 +15,25 @@ import java.io.IOException;
 public class SceneHandler {
 
     private static Scene sceneAccueil;
-
     private static Scene sceneJeu;
-
     private static SubScene subSceneMise;
-
     private static Scene sceneInscription;
     private static Scene sceneConnexion;
+    private static Scene sceneSettings;
+    private static Scene sceneRegles;
+
 
     private final static FXMLLoader jeuLoader = new FXMLLoader(BlackJackApplication.class.getResource("game-playerTurn-view.fxml"));
     private final static FXMLLoader accueilLoader = new FXMLLoader(BlackJackApplication.class.getResource("hello-view.fxml"));
     private final static FXMLLoader miseLoader = new FXMLLoader(BlackJackApplication.class.getResource("game-miseSelection-view.fxml"));
     private final static FXMLLoader inscriptionLoader = new FXMLLoader(BlackJackApplication.class.getResource("inscription-view.fxml"));
-
+    private final static FXMLLoader settingsLoader = new FXMLLoader(BlackJackApplication.class.getResource("settings-view.fxml"));
+    private final static FXMLLoader headerLoader = new FXMLLoader(BlackJackApplication.class.getResource("header.fxml"));
     private final static FXMLLoader connexionLoader = new FXMLLoader(BlackJackApplication.class.getResource("connexion-view.fxml"));
+    private final static FXMLLoader reglesLoader = new FXMLLoader(BlackJackApplication.class.getResource("regles-view.fxml"));
 
     private Stage stageApplication;
-
+    private VueGenerale vueGenerale;
     private static StackPane stackPane;
 
     public SceneHandler(Stage stage) throws IOException {
@@ -41,7 +43,11 @@ public class SceneHandler {
         sceneJeu = new Scene(jeuLoader.load(), 640, 700);
         sceneInscription = new Scene(inscriptionLoader.load(),640,700);
         sceneConnexion = new Scene(connexionLoader.load(),640,700);
+        sceneSettings = new Scene(settingsLoader.load());
+        sceneRegles = new Scene(reglesLoader.load());
 
+
+        vueGenerale = new VueGenerale(headerLoader, this);
 
         stackPane = new StackPane();
         stackPane.getChildren().add(sceneJeu.getRoot());
@@ -52,25 +58,48 @@ public class SceneHandler {
         stackPane.setBackground(backgroundJeu);
         sceneJeu.setRoot(stackPane);
 
-        stageApplication.setScene(sceneAccueil);
+        Scene sceneGenerale = new Scene(vueGenerale, 1000,700);
+        stageApplication.setScene(sceneGenerale);
+
+        afficherSettings();
         afficherAccueil();
     }
 
-    public void afficherAccueil(){
+    public void afficherAccueil() {
         ControleurAccueil controleurAccueil = accueilLoader.getController();
         controleurAccueil.initialiserAccueil(this);
+
+        vueGenerale.setCentre(sceneAccueil.getRoot());
     }
 
-    public void afficherInscription(){
-        stageApplication.setScene(sceneInscription);
+    public void afficherSettings() {
+
+        ControleurSettings controllerSettings = settingsLoader.getController();
+        controllerSettings.initialiserSettings(this);
+
+        vueGenerale.setCentre(sceneSettings.getRoot());
+    }
+
+    public void afficherInscription() {
         ControleurInscription controleurInscription = inscriptionLoader.getController();
         controleurInscription.initialiserInscription(this);
+
+        vueGenerale.setCentre(sceneInscription.getRoot());
     }
 
-    public void afficherConnexion(){
-        stageApplication.setScene(sceneConnexion);
+    public void afficherConnexion() {
         ControleurConnexion controleurConnexion = connexionLoader.getController();
         controleurConnexion.initialiserConnexion(this);
+
+        vueGenerale.setCentre(sceneConnexion.getRoot());
+    }
+
+    public void afficherRegles() {
+        ControleurRegles controleurRegles = reglesLoader.getController();
+        controleurRegles.initialiserRegles(this);
+
+        vueGenerale.setCentre(sceneRegles.getRoot());
+
     }
 
 
