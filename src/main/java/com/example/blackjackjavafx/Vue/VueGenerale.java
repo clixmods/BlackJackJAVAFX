d5e8aecba4465801f4c393bddaff440ba4920200
Bendahmane Yanis <yanis.bendahmane@etu.umontpellier.fr>
2024-01-17 09:38:40 +0100

    Les settings s'affichent par dessus la page actuelle (il s'agit de la branche settingsAsSubScene que j'aurais voulu merge ici mais qui a été impossible car de nouveaux commits ont été effectués dessus)

--- src/main/java/com/example/blackjackjavafx/Vue/VueGenerale.java
+++ src/main/java/com/example/blackjackjavafx/Vue/VueGenerale.java
@@ -1,23 +1,37 @@
 package com.example.blackjackjavafx.Vue;
 
 import com.example.blackjackjavafx.Application.controller.ControleurHeader;
+import com.example.blackjackjavafx.Application.controller.ControleurSettings;
 import com.example.blackjackjavafx.BlackJackApplication;
 import com.example.blackjackjavafx.Metier.Client;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.SubScene;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
 
 public class VueGenerale extends BorderPane {
 
+    private StackPane centre;
+
+    private SubScene parametres;
+
+    private ControleurSettings controleurSettings;
+
     private ControleurHeader controleurHeader;
 
-    public VueGenerale(FXMLLoader headerLoader, SceneHandler sceneHandler){
+    public VueGenerale(FXMLLoader headerLoader, FXMLLoader settingsLoader, SceneHandler sceneHandler){
+        centre = new StackPane();
+        setCenter(centre);
         try{
             setTop(headerLoader.load());
+            parametres = new SubScene(settingsLoader.load(), 800, 800);
             controleurHeader = headerLoader.getController();
             controleurHeader.initialiserHeader(sceneHandler);
+            controleurSettings = settingsLoader.getController();
+            controleurSettings.initialiserSettings(sceneHandler);
         }
         catch(IOException e){
             e.printStackTrace();
@@ -29,7 +43,16 @@ public class VueGenerale extends BorderPane {
     }
 
     public void setCentre(javafx.scene.Node centre){
-        setCenter(centre);
+        this.centre.getChildren().clear();
+        this.centre.getChildren().add(centre);
+    }
+
+    public void setParametres(){
+        centre.getChildren().add(parametres);
+    }
+
+    public void unsetParametres(){
+        centre.getChildren().remove(parametres);
     }
 
     public void miseAJourHeader(Client client){
