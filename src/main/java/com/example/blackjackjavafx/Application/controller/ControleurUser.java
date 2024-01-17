22e91d854ee0c2210ec92c00971629fd2405f9e5
Elliot B <119609060+Zorglub999@users.noreply.github.com>
2024-01-17 22:03:49 +0100

    Menu user

---
+++ src/main/java/com/example/blackjackjavafx/Application/controller/ControleurUser.java
@@ -0,0 +1,50 @@
+package com.example.blackjackjavafx.Application.controller;
+
+import com.example.blackjackjavafx.Application.connection.Connexion;
+import com.example.blackjackjavafx.Metier.Client;
+import com.example.blackjackjavafx.Vue.SceneHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ControleurUser implements Controleur{
+
+
+    @FXML
+    private Label labelNom;
+    @FXML
+    private Label labelPrenom;
+    @FXML
+    private Label labelEmail;
+    @FXML
+    private Label labelArgent;
+
+
+    private SceneHandler sceneHandler;
+    public void initialiserUser(SceneHandler sceneHandler){
+        this.sceneHandler = sceneHandler;
+        changerLangue();
+        updateLabels();
+    }
+    @Override
+    public void changerLangue() {
+
+    }
+
+    @FXML
+    public void handleAjouterArgent(){
+
+    }
+
+    @FXML
+    public void handleDeconnexion(){
+
+    }
+
+    private void updateLabels() {
+        Client client = Connexion.getInstance().getClientConnecte();
+        labelNom.setText("Nom: " + client.getNom());
+        labelPrenom.setText("Prénom: " + client.getPrenom());
+        labelEmail.setText("Mail: " + client.getMail());
+        labelArgent.setText("Argent sur le compte: $" + client.getArgent());
+    }
+}
