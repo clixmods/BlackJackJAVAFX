11c72e5b3a44d387a0c0d3955b127b65c1cd5287
Elliot B <119609060+Zorglub999@users.noreply.github.com>
2024-01-17 16:16:46 +0100

    implémentation de la musique + volume

--- src/main/java/com/example/blackjackjavafx/Application/controller/ControleurSettings.java
+++ src/main/java/com/example/blackjackjavafx/Application/controller/ControleurSettings.java
@@ -43,7 +43,7 @@ public class ControleurSettings implements Controleur{
         difficulte = difficulteBox.getValue();
 
 
-        System.out.println("Volume de la musique : " + volumeMusique);
+        sceneHandler.reglerVolumeMusique(volumeMusique);
         System.out.println("Volume des effets sonores : " + volumeEffets);
         System.out.println("Langue sélectionnée : " + langue);
         System.out.println("Difficulté sélectionnée : " + difficulte);
