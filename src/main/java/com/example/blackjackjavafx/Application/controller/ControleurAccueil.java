14627b35a8741c9698ae3aa4860500f3058ab55d
Bendahmane Yanis <yanis.bendahmane@etu.umontpellier.fr>
2024-01-17 16:30:02 +0100

    Ajout de boutons à l'accueil

--- src/main/java/com/example/blackjackjavafx/Application/controller/ControleurAccueil.java
+++ src/main/java/com/example/blackjackjavafx/Application/controller/ControleurAccueil.java
@@ -5,8 +5,13 @@ import com.example.blackjackjavafx.Application.connection.Connexion;
 import com.example.blackjackjavafx.Metier.Client;
 import com.example.blackjackjavafx.Vue.SceneHandler;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class ControleurAccueil implements Controleur{
+
+    @FXML
+    private Button inscriptionButton;
 
     private SceneHandler sceneHandler;
 
