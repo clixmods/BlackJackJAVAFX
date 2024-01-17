af70290cab93ec543916641534781ae6cc27c356
Bendahmane Yanis <yanis.bendahmane@etu.umontpellier.fr>
2024-01-17 15:09:37 +0100

    Le jeu peut désormais changer de langue. Ajout de la valeur de la main du croupier dans l'affichage

--- src/main/java/com/example/blackjackjavafx/Application/Langage/LangageFR.java
+++ src/main/java/com/example/blackjackjavafx/Application/Langage/LangageFR.java
@@ -28,6 +28,20 @@ public class LangageFR implements Langage{
         dictionnaire.put("mise_miseText_cantBet", "Vous ne pouvez jouer si vous ne misez pas.");
         dictionnaire.put("mise_miseText_notEnoughMoney", "Vous n'avez plus assez d'argent pour miser autant.");
         dictionnaire.put("mise_validerMiseButton", "Je valide ma mise");
+
+        dictionnaire.put("jeu_handDealerText", "Main du croupier : ");
+        dictionnaire.put("jeu_handPlayerText", "Main du joueur : ");
+        dictionnaire.put("jeu_messageRoundText_selectMise", "Sélectionnez votre mise");
+        dictionnaire.put("jeu_messageRoundText_cardDistribution", "Distribution des cartes");
+        dictionnaire.put("jeu_messageRoundText_playerTurn", "Tour joueur");
+        dictionnaire.put("jeu_messageRoundText_dealerTurn", "Tour croupier");
+        dictionnaire.put("jeu_messageRoundText_victory", "Vous avez gagné ! Vous remportez ");
+        dictionnaire.put("jeu_messageRoundText_draw", "Égalité !");
+        dictionnaire.put("jeu_messageRoundText_lose", "Vous avez perdu");
+        dictionnaire.put("jeu_standButton", "Rester");
+        dictionnaire.put("jeu_hitButton", "Piocher");
+        dictionnaire.put("jeu_doubleButton", "Doubler");
+        dictionnaire.put("jeu_restartButton", "Recommencer");
     }
 
     public static LangageFR getInstance(){
